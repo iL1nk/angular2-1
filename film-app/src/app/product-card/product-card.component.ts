1488c6e9f292f2a1836c27d862e21c21b1c38964
iL1nk <ilink@ua.fm>
2017-04-26 01:54:48 +0300

    edit

--- film-app/src/app/product-card/product-card.component.ts
+++ film-app/src/app/product-card/product-card.component.ts
@@ -12,9 +12,9 @@ export class ProductCardComponent implements OnInit {
   constructor() { }
 
   ngOnInit():void {
-    this.products = [
+		this.products = [
       {name: "Nikon", description: "Описание к фотоапарату Никон", url: "http://desktopwallpapers.org.ua/large/201702/56308.jpg"},
-      {name: "Текстура", description: "Описание к Текстуре", url: "http://desktopwallpapers.org.ua/large/201702/56309.jpg"},
+    	{name: "Текстура", description: "Описание к Текстуре", url: "http://desktopwallpapers.org.ua/large/201702/56309.jpg"},
       {name: "Архитектура", description: "Очень необычный дом", url: "http://desktopwallpapers.org.ua/large/201702/56311.jpg"},
       {name: "Природа", description: "Картина весны", url: "http://desktopwallpapers.org.ua/large/201702/56313.jpg"},
       {name: "Космос", description: "Обнаружена черная дыра", url: "http://desktopwallpapers.org.ua/large/201702/56314.jpg"}
